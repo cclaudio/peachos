a2ad48876b57f2e4cbabeb100ca4b135949d4c64
Claudio Carvalho <claudiodecarvalho@gmail.com>
2021-06-25 23:38:01 -0300

    Enter protected mode
    When we enter protected mode we can no longer access the BIOS interrupt
    handlers to do things like read from disk, hence we delete the print
    code.
    
    Define the GDT and enter protected mode.
    
    Signed-off-by: Claudio Carvalho <claudiodecarvalho@gmail.com>

--- Makefile
+++ Makefile
@@ -5,8 +5,10 @@
 # BIOS only handles binaries hence -f bin
 all:
 	nasm -f bin ./boot.asm -o ./boot.bin
-	dd if=./message.txt >> ./boot.bin
-	dd if=/dev/zero bs=512 count=1 >> ./boot.bin
+
+
+debug:
+	gdb -ex "target remote | qemu-system-x86_64 -hda ./boot.bin -S -gdb stdio"
 
 clean:
 	rm ./boot.bin
--- boot.asm
+++ boot.asm
@@ -2,8 +2,11 @@
 ;
 ; Author: Claudio Carvalho <claudiodecarvalho@gmail.com>
 
-ORG 0
+ORG 0x7c00
 BITS 16			; 16 bit architecture
+
+CODE_SEG equ gdt_code - gdt_start
+DATA_SEG equ gdt_data - gdt_start
 
 _start:
 	jmp short start
@@ -15,7 +18,7 @@ _start:
 times 33 db 0
 
 start:
-	jmp 0x7c0:step2		; Make our code segment 0x7c0
+	jmp 0x0:step2
 
 	; Before changing the segment registers we better disable interrupts (cli) to avoid
 	; any race condition. Then enable interrupts again (sti)
@@ -28,36 +31,66 @@ start:
 	; message = DS + offset
 step2:
 	cli
-	mov ax, 0x7c0
+	mov ax, 0x00
 	mov ds, ax
 	mov es, ax
-	mov ax, 0x00
 	mov ss, ax
 	mov sp, 0x7c00		; Stack grows downwards
 	sti
 
+; Enter protected mode
+; https://wiki.osdev.org/Protected_mode
+.load_protected:
+	cli
+	lgdt [gdt_descriptor]
+	mov eax, cr0
+	or eax, 0x1
+	mov cr0, eax
+	jmp CODE_SEG:load32	; Jumps to the code selector (CS) load32
+
+; Global Descriptor Table (GDT)
+gdt_start:
+gdt_null:
+	dd 0x0
+	dd 0x0
+
+; Offset 0x8
+gdt_code:		; CS should point to this
+	dw 0xffff	; Segment limit first 0-15 bits
+	dw 0		; Base first 0-15 bits
+	db 0		; Base 16-23 bits
+	db 0x9a		; Access byte
+	db 11001111b	; High 4 bit flags and the low 4 bit flags
+	db 0		; Base 24-31 bits
+
+; Offset 0x10
+gdt_data:		; DS, SS, ES, FS, GS
+	dw 0xffff	; Segment limit first 0-15 bits
+	dw 0		; Base first 0-15 bits
+	db 0		; Base 16-23 bits
+	db 0x92		; Access byte
+	db 11001111b	; High 4 bit flags and the low 4 bit flags
+	db 0		; Base 24-31 bits
+	
+gdt_end:
+
+gdt_descriptor:
+	dw gdt_end - gdt_start-1	; size field
+	dd gdt_start			; offset field
+
+; Every thing below this is considered 32 bits and we can no longer access
+; BIOS interrupt handlers
+[BITS 32]
+load32:
+	mov ax, DATA_SEG
+	mov ds, ax
+	mov es, ax
+	mov fs, ax
+	mov gs, ax
+	mov ss, ax
+	mov ebp, 0x00200000
+	mov esp, ebp
 	jmp $
-
-print:
-	mov bx, 0
-.loop:
-	lodsb
-	cmp al, 0
-	je .done
-	call print_char
-	jmp .loop
-.done:
-	ret
-
-; Call into BIOS interrupt call to print a character in the screen
-; int 10/AH=0eh
-; http://www.ctyme.com/rbrown.htm
-print_char:
-	mov ah, 0eh
-	int 0x10
-	ret
-
-error_message: db 'Failed to load sector', 0
 
 ; The BIOS will look for a bootable segment, which means 0x55AA in the last two
 ; bytes of the first 512 bytes segment. Remember that x86 is little endian,
