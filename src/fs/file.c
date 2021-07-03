92f79776ad696de1330d279c1245fd162824907d
Claudio Carvalho <claudiodecarvalho@gmail.com>
2021-07-03 00:18:35 -0300

    src/file: Add fseek to the VFS
    Signed-off-by: Claudio Carvalho <claudiodecarvalho@gmail.com>

--- src/fs/file.c
+++ src/fs/file.c
@@ -174,6 +174,16 @@ out:
     return res;
 }
 
+int fseek(int fd, int offset, FILE_SEEK_MODE whence)
+{
+    struct file_descriptor *desc = file_get_descriptor(fd);
+
+    if (!fd)
+        return -EIO;
+
+    return desc->filesystem->seek(desc->private, offset, whence);
+}
+
 int fread(void *ptr, uint32_t size, uint32_t nmemb, int fd)
 {
     struct file_descriptor *desc;
