a8a82d8736f67de8ef354a38568b59d9b8e1f56c
Claudio Carvalho <claudiodecarvalho@gmail.com>
2021-07-03 00:40:54 -0300

    src/file: Add fstat to the VFS
    Signed-off-by: Claudio Carvalho <claudiodecarvalho@gmail.com>

--- src/fs/file.c
+++ src/fs/file.c
@@ -174,6 +174,16 @@ out:
     return res;
 }
 
+int fstat(int fd, struct file_stat *stat)
+{
+    struct file_descriptor *desc = file_get_descriptor(fd);
+
+    if (!desc)
+        return -EIO;
+
+    return desc->filesystem->stat(desc->disk, desc->private, stat);
+}
+
 int fseek(int fd, int offset, FILE_SEEK_MODE whence)
 {
     struct file_descriptor *desc = file_get_descriptor(fd);
