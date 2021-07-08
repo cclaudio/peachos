59ed2dde27ac790f062a4e67923fa60125cc1b96
Claudio Carvalho <claudiodecarvalho@gmail.com>
2021-07-08 01:28:56 -0300

    src/kernel: Add terminal backspace

--- src/kernel.c
+++ src/kernel.c
@@ -43,6 +43,21 @@ uint16_t terminal_make_char(char c, char colour)
 	return (colour << 8) | c;
 }
 
+void terminal_backspace(void)
+{
+	if (terminal_row == 0 && terminal_col == 0)
+		return;
+
+	if (terminal_col == 0) {
+		terminal_row -= 1;
+		terminal_col = VGA_WIDTH;
+	}
+
+	terminal_col -= 1;
+	terminal_writechar(' ', 15);
+	terminal_col -= 1;
+}
+
 void terminal_putchar(int x, int y, char c, char colour)
 {
 	video_mem[(y * VGA_WIDTH) + x] = terminal_make_char(c, colour);
@@ -53,6 +68,11 @@ void terminal_writechar(char c, char colour)
 	if (c == '\n') {
 		terminal_row++;
 		terminal_col = 0;
+		return;
+	}
+
+	if (c == 0x08) {
+		terminal_backspace();
 		return;
 	}
 
