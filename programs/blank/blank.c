a3196a55c5593e951335efb4ee7a85297c8f790f
Claudio Carvalho <claudiodecarvalho@gmail.com>
2021-07-08 21:28:09 -0300

    programs/stdlib: Add system print

--- programs/blank/blank.c
+++ programs/blank/blank.c
@@ -1,4 +1,9 @@
+#include "peachos.h"
+
 int main(int argc, char **argv)
 {
+    print("Hello how are you?\n");
+
+    while(1) {}  // loop, we did not create exit yet
     return 0;
 }
---
+++ programs/stdlib/src/peachos.asm
@@ -0,0 +1,14 @@
+[BITS 32]
+
+global print:function
+
+; void print(const char *message)
+print:
+    push ebp
+    mov ebp, esp
+    push dword[ebp+8]
+    mov eax, 1      ; Command print
+    int 0x80
+    add esp, 4      ; Restore the stack pointer
+    pop ebp
+    ret
