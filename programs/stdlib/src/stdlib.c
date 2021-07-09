31890d35336a45017e6c72417764fa906ab00b2a
Claudio Carvalho <claudiodecarvalho@gmail.com>
2021-07-08 22:43:23 -0300

    programs/stdlib: Add itoa() function
    itoa() converts an integer to a string.
    E.g. itoa(8367) = '8367'

--- programs/stdlib/src/stdlib.c
+++ programs/stdlib/src/stdlib.c
@@ -1,6 +1,35 @@
 #include "peachos.h"
 #include "stdlib.h"
 
+char *itoa(int i)
+{
+    static char text[12];
+    char neg;
+    int loc;
+
+    loc = 11;
+    text[11] = 0;
+    neg = 1;
+
+    if (i >= 0) {
+        neg = 0;
+        i = -i;
+    }
+
+    while (i) {
+        text[--loc] = '0' - (i % 10);
+        i /= 10;
+    }
+
+    if (loc == 11)
+        text[--loc] = '0';
+
+    if (neg)
+        text[--loc] = '-';
+    
+    return &text[loc];
+}
+
 void *malloc(size_t size)
 {
     return peachos_malloc(size);
