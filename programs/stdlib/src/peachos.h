6b85dbd33d449a06dda757517da4470057ddc497
Claudio Carvalho <claudiodecarvalho@gmail.com>
2021-07-08 23:29:36 -0300

    programs/stdlib: Add peachos_terminal_readline()

--- programs/stdlib/src/peachos.h
+++ programs/stdlib/src/peachos.h
@@ -2,11 +2,14 @@
 #define PEACHOS_H
 
 #include <stddef.h>
+#include <stdbool.h>
 
 void print(const char *message);
-int getkey(void);
+int peachos_getkey(void);
 void *peachos_malloc(size_t size);
 void peachos_free(void *ptr);
 void peachos_putchar(char c);
+int peachos_getkeyblock(void);
+void peachos_terminal_readline(char *out, int max, bool output_while_typing);
 
 #endif // PEACHOS_H
